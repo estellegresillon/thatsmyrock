d6aec057b4265851873996043426ec72b1ac4f6a
estellegresillon <estelle.gresillon@gmail.com>
2018-05-28 17:11:48 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'albums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :albums, only: [ :index, :show ] do
+    resources :user_albums, only: [ :create, :update ]
+  end
+
+  resources :user_albums, only: [ :index, :show ]
+  resources :artists, only: [ :show, :index ]
+
 end
