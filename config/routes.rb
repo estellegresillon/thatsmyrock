b82098ed860d586afd329f9da19b74e0c9b718e2
Benjamin Debray <shouty91600@hotmail.fr>
2018-05-28 18:45:10 +0200

    changement routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'albums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :albums, only: [ :index, :show ] do
-    resources :user_albums, only: [ :create, :update ]
+    resources :user_albums, only: [ :create ]
   end
 
-  resources :user_albums, only: [ :index, :show ]
+  resources :user_albums, only: [ :index, :show, :update, :destroy ]
   resources :artists, only: [ :show, :index ]
 
 end
