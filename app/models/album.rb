dabc7129211f8c6911c7ad3aced7e7e85b35bb7a
estellegresillon <estelle.gresillon@gmail.com>
2018-05-29 17:18:02 +0200

    index and show

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,16 @@ class Album < ApplicationRecord
   has_many :tracks, dependent: :destroy
   belongs_to :artist
   has_many :user_albums, dependent: :destroy
+
+  scope :next, lambda {|id| where("id > ?",id).order("id ASC") } # this is the default ordering for AR
+  scope :previous, lambda {|id| where("id < ?",id).order("id DESC") }
+
+  def next
+    Album.next(self.id).first
+  end
+
+  def previous
+    Album.previous(self.id).first
+  end
+
 end
