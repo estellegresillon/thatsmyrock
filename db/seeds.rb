cc5f0d79bd27682466c90eb2046ffd817fdeb8d6
Olivier Reynaud <olivreynaud@gmail.com>
2018-05-29 13:02:52 +0200

    seed started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_options = {
+  col_sep: ',',
+  quote_char: '"',
+  headers: :first_row
+}
+
+filepath    = 'artists.csv'
+CSV.open(filepath, 'wb', csv_options) do |csv|
+  csv << ['name',
+           'country',
+           'facebook_url',
+           'instagram_url',
+           'twitter_url',
+           'website_url',
+           'wiki_url',
+           'bio',
+           'photo'
+   ]
+
+
+csv_options = {
+  col_sep: ',',
+  quote_char: '"',
+  headers: :first_row
+}
+
+filepath    = 'albums.csv'
+CSV.open(filepath, 'wb', csv_options) do |csv|
+  csv << ['artist', 'rank', 'name', 'year', 'music_style', 'wiki_url', 'photo_cover', 'description']
+
+csv_options = {
+  col_sep: ',',
+  quote_char: '"',
+  headers: :first_row
+}
+
+filepath    = 'tracklists.csv'
+CSV.open(filepath, 'wb', csv_options) do |csv|
+  csv << ['album', 'tracklist_order', 'name', 'duration']
+end
+
+
+
+# artists
+# name;country;facebook_url;instagram_url;twitter_url;website_url;wiki_url;bio;photo
+#     t.string "name"
+#     t.string "facebook_url"
+#     t.string "wiki_url"
+#     t.text "bio"
+#     t.string "instagram_url"
+#     t.string "twitter_url"
+#     t.string "photo"
+#     t.string "country"
+
+# albums
+# artist;rank;name;year;music_style;wiki_url;photo_cover;description
+#     t.string "name"
+#     t.text "description"
+#     t.string "music_style"
+#     t.integer "year"
+#     t.integer "rank"
+#     t.string "wiki_url"
+#     t.string "photo_cover"
+#     t.string "photo_show"
+
+# trakclists
+# album;tracklist_order;name;duration
+#     t.string "name"
+#     t.bigint "album_id"
+#     t.integer "duration"
