04b847734800cb4d8b99f6cb299233176c9adeba
Olivier Reynaud <olivreynaud@gmail.com>
2018-05-30 17:00:36 +0200

    many cloudinary bug fix, url update, good synch database + cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,40 +8,39 @@ csv_options = {
   header_converters: :symbol
 }
 
-puts "Cleaning database..."
-Artist.destroy_all
+# puts "Cleaning database..."
+# Artist.destroy_all
 
-puts "Creating artists..."
-filepath = Rails.root.join('db/fixtures/csv/artists.csv')
+# puts "Creating artists..."
+# filepath = Rails.root.join('db/fixtures/csv/artists.csv')
 
-CSV.foreach(filepath, csv_options) do |row|
-  artist = Artist.new(
-    name: row[:name],
-    country: row[:country],
-    facebook_url: row[:facebook_url],
-    instagram_url: row[:instagram_url],
-    twitter_url: row[:twitter_url],
-    website_url: row[:website_url],
-    wiki_url: row[:wiki_url],
-    bio: row[:bio]
-    # photo: row[:photo],
-  )
+# CSV.foreach(filepath, csv_options) do |row|
+#   artist = Artist.new(
+#     name: row[:name],
+#     country: row[:country],
+#     facebook_url: row[:facebook_url],
+#     instagram_url: row[:instagram_url],
+#     twitter_url: row[:twitter_url],
+#     website_url: row[:website_url],
+#     wiki_url: row[:wiki_url],
+#     bio: row[:bio]
+#   )
 
-  begin
-    # photo_title = "#{row[:name].downcase.gsub(' ', '-')}"
-    photo_title = I18n.transliterate(row[:name].gsub('&', 'and').parameterize)
-    photo = Cloudinary::Uploader.upload("db/fixtures/images/artists/#{photo_title}.jpg", :use_filename => true, :folder => "thatsmyrock/artists")
-    artist.remote_photo_url = photo['url']
-  rescue
-    puts "No photo for #{row[:name]}"
-  end
+#   # photo_title = "#{row[:name].downcase.gsub(' ', '-')}"
+#   photo_title = I18n.transliterate(row[:name].gsub('&', 'and').parameterize)
+#   filepath    = Rails.root.join("db/fixtures/images/artists/#{photo_title}.jpg")
 
-    row << ['Name', 'Appearance', 'Origin']
-    row << ['Asahi', 'Pale Lager', 'Japan']
-    row << ['Guinness', 'Stout', 'Ireland']
+#   if File.exist?(filepath)
+#     # First, we upload the photo on cloudinary with options to keep filename and have specific folder
+#     photo = Cloudinary::Uploader.upload(filepath, use_filename: true, folder: "thatsmyrock/artists")
 
-    artist.save!
-end
+#     # Then, we use the public_id that was returned in photo variable
+#     # We have to set directly the attribute with [:photo] otherwise it uses the uploader and uploads a second time
+#     artist[:photo] = "image/upload/#{photo['public_id']}"
+#   end
+
+#   artist.save!
+# end
 
 
 
@@ -50,11 +49,12 @@ puts "Creating albums..."
 filepath = Rails.root.join('db/fixtures/csv/albums.csv')
 
 CSV.foreach(filepath, csv_options) do |row|
-  # album-name-artist-name.jpg
+  # REFERENCE: album-name-artist-name.jpg
 
   # 1) Trouver l'artiste correspondatn au nom d'artiste dans le csv
-  puts row[:artist]
+  # puts row[:artist]
   artist = Artist.find_by(name: row[:artist])
+
   # 2) Créer un album avec l'artiste_id correspondant
   album = Album.new(
     artist: artist,
@@ -64,20 +64,29 @@ CSV.foreach(filepath, csv_options) do |row|
     music_style: row[:music_style],
     wiki_url: row[:wiki_url],
     description: row[:description]
-    # photo_cover: row[:photo_cover],
-    # photo_show: row[:photo_show],
   )
-  begin
-    # photo_title = "#{row[:name]}-#{row[:artist]}".downcase.gsub(' ', '-')
-    photo_title = "#{I18n.transliterate(row[:name].gsub('&', 'and').parameterize)}-#{I18n.transliterate(row[:artist].gsub('&', 'and').parameterize)}"
-    photo_cover = Cloudinary::Uploader.upload("db/fixtures/images/artists/#{photo_title}.jpg", :use_filename => true, :folder => "thatsmyrock/albums")
-    album.remote_photo_cover_url = photo_cover['url']
-    photo_show = Cloudinary::Uploader.upload("db/fixtures/images/artists/#{photo_title}-show.jpg", :use_filename => true, :folder => "thatsmyrock/albums")
-    album.remote_photo_show_url = photo_show['url']
-  rescue
-    puts "No album photo for #{row[:name]}"
+
+  # photo_title = "#{row[:name]}-#{row[:artist]}".downcase.gsub(' ', '-')
+  photo_title = "#{I18n.transliterate(row[:name].gsub('&', 'and').parameterize)}-#{I18n.transliterate(row[:artist].gsub('&', 'and').parameterize)}"
+
+  filepath_cover = Rails.root.join("db/fixtures/images/albums/#{photo_title}.jpg")
+  filepath_show = Rails.root.join("db/fixtures/images/albums/#{photo_title}-show.jpg")
+
+  if File.exist?(filepath_cover)
+    # First, we upload the photo on cloudinary with options to keep filename and have specific folder
+    photo_cover = Cloudinary::Uploader.upload(filepath_cover, use_filename: true, folder: "thatsmyrock/albums")
+
+    # Then, we use the public_id that was returned in photo variable
+    # We have to set directly the attribute with [:photo] otherwise it uses the uploader and uploads a second time
+    album[:photo_cover] = "image/upload/#{photo_cover['public_id']}"
   end
-    album.save!
+
+  if File.exist?(filepath_show)
+    photo_show = Cloudinary::Uploader.upload(filepath_show, use_filename: true, folder: "thatsmyrock/albums")
+    album[:photo_show] = "image/upload/#{photo_show['public_id']}"
+  end
+
+  album.save!
 end
 
 puts "Creating tracklists..."
