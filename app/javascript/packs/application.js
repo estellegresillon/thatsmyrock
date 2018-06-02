5ef9a5ca6b4a9914a24eb1b55ea29fe2bdd20244
Benjamin Debray <shouty91600@hotmail.fr>
2018-06-02 10:22:02 +0200

    tooltip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
 import "bootstrap";
 import "jquery";
 import { tabActive } from '../components/tabs';
+import { toolTip } from '../components/tooltip';
 
 
 tabActive();
+if(document.getElementById("my-albums")){
+  toolTip();
+};
+
