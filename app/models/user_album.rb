3aed0f7094ad73c9730f58d7a74b4a96d5862298
Benjamin Debray <shouty91600@hotmail.fr>
2018-06-04 11:57:37 +0200

    searchbar:):):):):):)

--- app/models/user_album.rb
+++ app/models/user_album.rb
@@ -1,6 +1,10 @@
 class UserAlbum < ApplicationRecord
   belongs_to :user
   belongs_to :album
+  validates_uniqueness_of :user_id, :scope => [:album_id]
+
+  #todo add validation unicity of album_id and user_id
+
 
 
   def rank
