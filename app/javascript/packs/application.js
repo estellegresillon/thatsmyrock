96b1f5c7d33da21a9be72df6ed98e89afa5fde2c
Benjamin Debray <shouty91600@hotmail.fr>
2018-06-06 17:12:04 +0200

    format profile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@ import "bootstrap";
 import "jquery";
 import { tabActive } from '../components/tabs';
 import { toolTip } from '../components/tooltip';
-
+import { popOver } from '../components/tooltip';
 
 tabActive();
 toolTip();
 global.toolTip = toolTip;
+popOver();
