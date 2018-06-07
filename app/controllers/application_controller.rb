2436d12c517571a00fc94d3b443425d0277685da
Olivier Reynaud <olivreynaud@gmail.com>
2018-06-07 09:37:03 +0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
+  # app/controllers/application_controller.rb
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
 end
